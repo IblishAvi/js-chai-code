efc88a0937e5bb5397adc94095758f0fe6d6a87b
Avinash Jha <127582620+IblishAvi@users.noreply.github.com>
2025-02-03 16:42:05 +0000

    Variable in JavaScript

---
+++ 01_Basics/01_Variable.js
@@ -0,0 +1,11 @@
+const UserId = '01'
+let UserName = "AviJha"
+var UserState =" Active"
+UserCity ="Jaipur"
+
+// const UserId ="02" //Not allowed to Change Constant variable.
+UserName = 'Rishu Jha'
+UserState = "InActive"
+UserCity = "Delhi"
+
+// It is now prefferable to use Var for good Practice in tranding js Code
