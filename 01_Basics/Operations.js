621303be09499778944b318280b0bb641025b10f
Avinash Jha <127582620+IblishAvi@users.noreply.github.com>
2025-02-10 17:33:51 +0000

    more operations and comparisons

--- 01_Basics/Operations.js
+++ 01_Basics/Operations.js
@@ -24,11 +24,27 @@ let y = --b
 let x2 = a++
 let y2 = b--
 
-console.log("Pre Increment of",x); // 13
-console.log("Pre Decrement of",y); // 14
-console.log("Post Decrement of",x2); // 13
-console.log("P0st Decrement of",y2); //14
+// console.log("Pre Increment of",x); // 13
+// console.log("Pre Decrement of",y); // 14
+// console.log("Post Decrement of",x2); // 13
+// console.log("P0st Decrement of",y2); //14
 
 
+// ******************* Comparison ******************************
+console.log(a);
+console.log(b);
 
+
+// console.log("Greater ",a>b); // true
+// console.log("Smaller ",a<b); // false
+// console.log("Equals ",a==b); // false
+// console.log("Equals ",a===b); // false
+// console.log("Equals ",a!=b); // true
+
+// console.log(null == 0);
+// console.log(null >= 0); // Comparison of diffenrent type always gives the unexpected Result
+// console.log(null <= 0);  // Comparison of diffenrent type always gives the unexpected Result
+
+console.log( "2 " + 2 + 1); //first value is string so all the value will be concatinate by considering as String
+console.log( 2 + 2 + "5"); // here it gives the different result because first two letter is Integer and last one is String so last will be concatinated after Sum of first two.
 
