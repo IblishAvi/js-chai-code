862b62f94154af122645eef624a133668cbc124d
Avinash Jha <127582620+IblishAvi@users.noreply.github.com>
2025-02-10 17:49:17 +0000

    different primitive and non primitive datatype in JS

--- 01_Basics/02_DataType.js
+++ 01_Basics/02_DataType.js
@@ -8,6 +8,8 @@ let IsLoggedIn = true;
 let gender = null;
 let height = undefined;
 
+// Primitive Types in Javascript.
+
 // Number  holds value upto 2 to the power 52
 // BigInt
 // string
@@ -19,3 +21,41 @@ let height = undefined;
 
 console.log(typeof undefined)  //undefined
 console.log(typeof null) //object
+
+let num = 10;
+let num2 = 20.5;
+let BigInt = 54852166711n;
+let IsTrue = false;
+let IsFalse = true;
+let WeatherTemp = null;
+let myCar ;
+
+console.table([num ,typeof num,num2,typeof num2,BigInt , typeof BigInt,IsTrue, typeof IsTrue])
+
+// Non primitive (Reference)
+
+// Array, Object , Function
+
+let Employee = ['Virat', 'Avi', "Rishu", "Vivek"," Guddu"]
+console.log(Employee);
+console.log(typeof Employee); //object
+
+let profile = {
+    name:'Avi',
+    Age : '25',
+    gender:"Male"
+}
+
+console.log(profile);
+console.log(typeof profile); //object
+
+
+
+const MyHobby = function(){
+    console.log("Love to have ...");
+    
+}
+console.log(MyHobby);
+console.log( typeof MyHobby); //Function
+
+
