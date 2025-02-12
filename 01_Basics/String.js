d221464f93aa53befab26afdac59896d00862151
Avinash Jha <127582620+IblishAvi@users.noreply.github.com>
2025-02-12 16:23:01 +0000

    String and their Function.

---
+++ 01_Basics/String.js
@@ -0,0 +1,18 @@
+// String in javascript.String Can be defined in Two ways.
+// First Method
+let name = 'Avi';
+let Lname = 'Jha';
+// Second Method using New Keyword.
+const Fname = new String('Avinash');
+
+console.log(name);
+console.log(Fname);
+console.log(Fname[0]);
+
+// Methods In String
+console.log(Fname.concat(Lname));
+
+console.log(Fname.includes('A'));
+
+
+
