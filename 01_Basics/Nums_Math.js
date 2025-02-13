85cb8fde1271bfcb8fde98647498e8b1b6078f2b
Avinash Jha <127582620+IblishAvi@users.noreply.github.com>
2025-02-13 16:01:21 +0000

    Maths Function is Useful Everywhere.

---
+++ 01_Basics/Nums_Math.js
@@ -0,0 +1,43 @@
+// Numbers in Js
+let score = 50;
+let balance = new Number(100);
+// console.log(score); // 50 
+// console.log(balance); // [Number: 100]
+// console.log(balance.toString()); //  100
+// console.log(balance.toString().length); //  3
+// console.log(balance.toFixed(1)); //100.0
+// console.log(balance.toFixed(2)); //100.00
+let balance2 = new Number(100.890);
+
+// console.log(balance2.toPrecision()); //100
+
+
+
+
+
+// ++++++++++++++++++++++++++++++++++++++  Math ++++++++++++++++++++++++++++++++++++++++++++++
+
+console.log(Math);
+// let num = -4;
+// Methods In Math 
+// Abs always returns the positive Number 
+// console.log(Math.abs(-4));  // 4 
+// Round Method always returns the RoundOff value of Given no based on defined rule where if the precision value is greater than or equals 5 it Increase to nearest ten or else.
+// console.log(Math.round(4.68)); // 5
+// console.log(Math.round(4.20)); // 4
+// Ceil Method Returns the Ceil(Highest) value of the given no.
+console.log(Math.ceil(4.20)); // 5
+
+// floor Method Returns the floor(Lowest) value of the given no.
+console.log(Math.floor(4.20)); // 4
+console.log(Math.floor(4.90)); // 4
+
+// Random  No it gives the Random Value between 0 and 1.
+
+
+
+
+
+
+
+
