72effc1a284f341eac4c0c95133e733e83f48300
Avinash Jha <127582620+IblishAvi@users.noreply.github.com>
2025-02-23 17:13:36 +0000

    Comparison of String

--- 01_Basics/String.js
+++ 01_Basics/String.js
@@ -3,16 +3,36 @@
 let name = 'Avi';
 let Lname = 'Jha';
 // Second Method using New Keyword.
-const Fname = new String('Avinash');
+// const Fname = new String('Avinash');
 
-console.log(name);
-console.log(Fname);
-console.log(Fname[0]);
+// console.log(name);
+// console.log(Fname);
+// console.log(Fname[0]);
 
 // Methods In String
-console.log(Fname.concat(Lname));
+// console.log(Fname.concat(Lname));
 
-console.log(Fname.includes('A'));
+// console.log(Fname.includes('A'));
 
+const Animal = "cat";
+// console.log(Animal.charAt(1)); // a
+// console.log(Animal.toUpperCase()); // CAT 
+
+// Comparison of String 
+
+const a = 'a';
+const b = 'b';
+ if (a<b){
+    console.log(`${a} is less than ${b} `);
+    
+ }
+ else if (a > b){
+    console.log(`${a} is Greater than ${b} `);
+
+ }
+ else{
+    console.log(`${a} is Equal to ${b} `);
+    
+ }
 
 
