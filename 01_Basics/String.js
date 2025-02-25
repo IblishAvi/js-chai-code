8a5ec85f1a8481ac6617d727f15e2d4ffef9a3bc
Avinash Jha <127582620+IblishAvi@users.noreply.github.com>
2025-02-25 15:24:56 +0000

    String

--- 01_Basics/String.js
+++ 01_Basics/String.js
@@ -20,22 +20,22 @@ const Animal = "cat";
 
 // Comparison of String 
 
-const a = 'a';
-const b = 'b';
- if (a<b){
-    console.log(`${a} is less than ${b} `);
+// const a = 'a';
+// const b = 'b';
+//  if (a<b){
+//     console.log(`${a} is less than ${b} `);
     
- }
- else if (a > b){
-    console.log(`${a} is Greater than ${b} `);
+//  }
+//  else if (a > b){
+//     console.log(`${a} is Greater than ${b} `);
 
- }
- else{
-    console.log(`${a} is Equal to ${b} `);
+//  }
+//  else{
+//     console.log(`${a} is Equal to ${b} `);
     
- }
+//  }
 
-const sentense = 'A quick brown fox jumps over the lazy dog.';
+// const sentense = 'A quick brown fox jumps over the lazy dog.';
 
 // console.log(sentense.endsWith("fox")); //false
 // console.log(sentense.endsWith("Dog.")); // false
@@ -44,8 +44,21 @@ const sentense = 'A quick brown fox jumps over the lazy dog.';
 // console.log(sentense.indexOf("dog.")); // 39
 // console.log(sentense.indexOf("A")); // 0
 // console.log(sentense.indexOf("f")); // 14
-console.log(sentense.replace("quick","kuicq")); //A kuicq brown fox jumps over the lazy dog.
-console.log(sentense); //A quick brown fox jumps over the lazy dog.
+// console.log(sentense.replace("quick","kuicq")); //A kuicq brown fox jumps over the lazy dog.
+// console.log(sentense); //A quick brown fox jumps over the lazy dog.
 
+const str1 = 'I am born and brought up in bihar';
+const str2 = " I have very high expectation from Life."
+const areEqualInUpperCase = (str1, str2) =>
+   str1.toUpperCase() === str2.toUpperCase();
+ const areEqualInLowerCase = (str1, str2) =>
+   str1.toLowerCase() === str2.toLowerCase();
+ 
+ console.log(areEqualInUpperCase("ß", "ss")); // true; should be false
+ console.log(areEqualInLowerCase("ı", "I"));   // false; should be true
+ 
+
+
+ 
 
 
