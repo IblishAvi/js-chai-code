ef5c8f8ac7fadf028c335cdc51f76379f3645118
Avinash Jha <127582620+IblishAvi@users.noreply.github.com>
2025-02-28 16:31:27 +0000

    Date and time

--- 01_Basics/Date.js
+++ 01_Basics/Date.js
@@ -1,2 +1,13 @@
 const date = Date.now();
-console.log(date);
+// console.log(date);
+const mydate = new Date();
+// console.log(mydate.toString()); //Fri Feb 28 2025 15:19:01 GMT+0000 (Coordinated Universal Time)
+// console.log(mydate.getDate());
+
+let customDate = new Date(27,1,2025)
+let customDate2 = new Date("01-14-2025");
+// console.log(customDate.toDateString()); // Wed Aug 17 1932/
+console.log(customDate2.getFullYear()); // 2025
+// console.log(customDate.getTime()); 
+
+// console.log(mydate.getTime());
