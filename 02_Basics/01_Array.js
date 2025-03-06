c43b5858cdb1aedf0b48993419f902646d6e9951
Avinash Jha <127582620+IblishAvi@users.noreply.github.com>
2025-03-06 17:30:56 +0000

    More Methods In Array.

--- 02_Basics/01_Array.js
+++ 02_Basics/01_Array.js
@@ -12,13 +12,20 @@ const user = ["Avi","Rishu","Raja","Vivek"]
 const merge = user.join(arr)
 console.log(merge);
 const newArr = NewArr.concat(user)
-console.log(newArr);
-console.log(newArr);
-newArr.push(15)
-newArr.push(true)
-console.log(newArr);
+// console.log(newArr);
+// console.log(newArr);
+// newArr.push(15)
+// newArr.push(true)
+// console.log(newArr);
 newArr.unshift(20)
-console.log(newArr);
+// console.log(newArr);
 
-newArr.splice(2,2)
-console.log(newArr);
+newArr.splice(0,0,"Ananya")
+// console.log(newArr);
+
+newArr.splice(1,1,"Neha")
+// console.log(newArr);
+newArr.splice(1,1,"Shreya",'Shubham Pandey')
+// console.log(newArr);
+const Array2 =  newArr.toShorted()
+console.log(Array2);
