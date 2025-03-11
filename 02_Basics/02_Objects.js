1dd92bd081dd4fe56ae1e97e0c871447fc740bd3
Avinash Jha <127582620+IblishAvi@users.noreply.github.com>
2025-03-11 16:37:14 +0000

    Object in Methods

---
+++ 02_Basics/02_Objects.js
@@ -0,0 +1,30 @@
+// object sigleton
+
+
+// object literal
+const Myhobby = Symbol('key1');
+const JsUser = {
+    name : "Avi",
+    Age: 28,
+    [Myhobby] :"Book",
+    "Firstname" :"Avi",
+    Location:"Hajipur",
+    email:"Avi@gmail.com",
+    isLoggedin:'False',
+    FulluserName:{
+        username:" Avi",
+        Title : 'Jha'
+    }
+
+}
+console.log(JsUser);
+// Object.freeze(JsUser)
+JsUser.Age= 30;
+console.log(JsUser);
+console.log(JsUser.Firstname);
+
+JsUser.getname = function (){
+    console.log('Avi Js Expert');
+    
+}
+console.log(JsUser.getname);
