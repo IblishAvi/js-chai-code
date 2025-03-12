108ed1e59cb0fbdf643978e0229dabfa3bf8584e
Avinash Jha <127582620+IblishAvi@users.noreply.github.com>
2025-03-12 15:22:33 +0000

    Methods and Function of objects

--- 02_Basics/02_Objects.js
+++ 02_Basics/02_Objects.js
@@ -33,4 +33,56 @@ const tinderuser = new Object();
 
 tinderuser.id = '123abd';
 tinderuser.email = 'Avi@gmail.com';
-console.log(tinderuser);
+tinderuser.FulluserName = {
+    username:{
+        Firstname:'Avinash',
+        Lastname:'Jha'
+    }
+}
+// console.log(tinderuser.FulluserName.username.Firstname);
+
+
+const obj1 = { 1:'a',2:'b'}
+const obj2 = { 3:'c',4:'d'}
+
+// const obj3 = Object.assign(obj1,obj2) 
+// console.log(obj3); //{ '1': 'a', '2': 'b', '3': 'c', '4': 'd' }
+
+// Spread Operator in Javascript.
+const obj3 = {...obj1,...obj2}
+// console.log(typeof obj3);
+const obj4 = {...obj3}
+// console.log(obj4);
+// console.log(typeof obj4);
+
+
+// console.log(obj3); //{ '1': 'a', '2': 'b', '3': 'c', '4': 'd' }
+
+
+// Array Of Objects 
+
+const userDetail = [
+    {
+        id:'123a',
+        name:'Avi',
+        Location:'Gujarat',
+    },
+    {
+        id:'123a',
+        name:'Avi',
+        Location:'Gujarat',
+    },
+    {
+        id:'123a',
+        name:'Avi',
+        Location:'Gujarat',
+    },
+    {
+        id:'123a',
+        name:'Avi',
+        Location:'Gujarat',
+    }
+]
+
+console.log(userDetail);
+
--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 
                                             Date:
 The Date.parse() static method parses a string representation of a date, and returns the date's timestamp.
+
+
+                                      Objects
+Object is a very vast topic in Javascript according to me ,it is because it has lot of methods and 
+the Features such as Spread operator and Array of object which will further help to learn API handling. 
