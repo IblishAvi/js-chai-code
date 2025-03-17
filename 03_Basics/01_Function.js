ee2cd7f3b3941390389d227e7cb330739e8144dc
Avinash Jha <127582620+IblishAvi@users.noreply.github.com>
2025-03-17 16:34:22 +0000

    function Introduction

---
+++ 03_Basics/01_Function.js
@@ -0,0 +1,17 @@
+function mydetail(num1 ,num2){
+    console.log('Hii, I am avi');
+    return num1+num2;
+}
+// console.log(mydetail(4,6))
+
+function restFunc(...num1){
+    return num1;
+}
+
+console.log(restFunc(200,300,400));
+
+const users = {
+    username:'Avi',
+    id:'00123a',
+
+}
