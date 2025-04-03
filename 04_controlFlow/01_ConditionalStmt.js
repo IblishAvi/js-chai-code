2053ecc7d00db9d559a659f18f1bc7e88830b5ec
Avinash Jha <127582620+IblishAvi@users.noreply.github.com>
2025-04-03 17:37:16 +0000

    ladder else if Statement

--- 04_controlFlow/01_ConditionalStmt.js
+++ 04_controlFlow/01_ConditionalStmt.js
@@ -1,39 +1,70 @@
-const LoggedIn = false;
-if(LoggedIn){
-    console.log('Please Login First');
+// const LoggedIn = false;
+// if(LoggedIn){
+//     console.log('Please Login First');
     
-}
-else{
-    function user(uname = 'Avi'){
-        console.log(`${uname} Login Successfully`);
+// }
+// else{
+//     function user(uname = 'Avi'){
+//         console.log(`${uname} Login Successfully`);
         
-    }
-    user('Rishu')
+//     }
+//     user('Rishu')
+// }
+
+// // == , < , >,  <= , >= , === ,!=
+
+// let val1 = 20;
+// let val2 = 25;
+
+// if(val1 === val2){
+//     console.log("Value is Equal:");
+    
+// }
+
+// if(val1 != val2){
+//     console.log("Value is not Equal:");
+    
+// }
+// if(val1 > val2){
+//     console.log("Value1  is Greater :");
+    
+// }
+// if(val1 < val2){
+//     console.log("Value1 is Lesser:");
+    
+// }
+// if(val1 <= val2){
+//     console.log("Value1 is Lessthan Equal:");
+    
+// }
+const Age= 18;
+const weight = 70;
+const heightInFeet = 6;
+
+if( Age<18){
+    if(weight>60 && weight <=70 && heightInFeet >6)
+        console.log("Player isnot fit for Selection")
+        
 }
 
-// == , < , >,  <= , >= , === ,!=
+else if(Age>=18)
+{
+    if(weight>60 && weight !=70 && heightInFeet!=6)
+    {
+    console.log("Player is not meeting Criterian")
+}
+console.log("Player is not fit for Selection")
 
-let val1 = 20;
-let val2 = 25;
+}
+else if(Age ===18)
+{
+    if(weight===70 && heightInFeet ===6)
+        console.log("Player is Fit For Selection");
 
-if(val1 === val2){
-    console.log("Value is Equal:");
+        
+}
+else {
+    console.log("Player should Try Again");
     
 }
-
-if(val1 != val2){
-    console.log("Value is not Equal:");
-    
-}
-if(val1 > val2){
-    console.log("Value1  is Greater :");
-    
-}
-if(val1 < val2){
-    console.log("Value1 is Lesser:");
-    
-}
-if(val1 <= val2){
-    console.log("Value1 is Lessthan Equal:");
-    
-}
+console.log("Hii")
