eed35c76bd92a89f0f52be6e00a7e128f049536d
Avinash Jha <127582620+IblishAvi@users.noreply.github.com>
2025-04-05 18:19:17 +0000

    Loop

---
+++ Loop/one.js
@@ -0,0 +1,18 @@
+// for (let i = 0; i < 5; i++) {
+//     console.log(`Outer Loop : ${i}`);
+    
+//     for (let j = 0; j <=i; j++) {
+//         console.log(`Inner Loop : ${j} and inner Loop ${i}`);
+        
+//     }
+// }
+
+for (let i = 0; i < 5; i++) {
+        for (let j = 0; j < i; j++) {
+            console.log(i );
+            
+            
+        }
+        console.log(" ");
+        
+}  
