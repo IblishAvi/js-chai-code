a1d7e2e13b6be25f6e0ee040dcd89a3062e8f718
Avinash Jha <127582620+IblishAvi@users.noreply.github.com>
2025-04-09 18:38:46 +0000

    more in loop

--- Loop/one.js
+++ Loop/one.js
@@ -7,15 +7,15 @@
 //     }
 // }
 
-for (let i = 0; i < 5; i++) {
-        for (let j = 0; j < i; j++) {
-            console.log(i );
+// for (let i = 0; i < 5; i++) {
+//         for (let j = 0; j < i; j++) {
+//             console.log(i );
             
             
-        }
-        console.log(" ");
+//         }
+//         console.log(" ");
         
-}  
+// }  
 //  Table in js
 
 // for (let i = 1; i <=5; i++) {
@@ -28,33 +28,43 @@ for (let i = 0; i < 5; i++) {
 
 // Trying to print pattern 
 
-let i = 1
-for ( ; i <=5 ; ) {
-    let j = 1
-    for (; j <=5; ) {
-        console.log(i)
-        j++
-    }
-    i++;
-    //   console.log(end = "");
-}
+// let i = 1
+// for ( ; i <=5 ; ) {
+//     let j = 1
+//     for (; j <=5; ) {
+//         console.log(i)
+//         j++
+//     }
+//     i++;
+//     //   console.log(end = "");
+// }
 
 
 // break and Continue
 
-// for (let index = 1; index < 10; index++) {
-//     if(index == 5){
-//         console.log("value found:");
-//         break
-        
-//     }
-    
-// }
 for (let index = 1; index < 10; index++) {
     if(index == 5){
-        console.log("value found:");
+        console.log(`Value found ${index}`);
+        break
+        
+    }
+     console.log(index);
+}
+
+for (let index = 1; index < 10; index++) {
+    if(index == 5){
+        console.log(`Value skipped ${index}`);
         continue
         
     }
+    console.log(index);
+    
+}
+
+const myArray = [ 'Avi', 26 ," male", "avi@gmail.com"]
+
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    console.table(element);
     
 }
